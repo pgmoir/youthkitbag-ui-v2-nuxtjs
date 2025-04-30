76851de7192ab39ebb0f26f519f5f496c687d50c
Philip Moir <phil@captur.ai>
2025-05-01 00:36:44 +0100

    Add description to metadata

--- app.vue
+++ app.vue
@@ -13,5 +13,11 @@ useHead({
     titleChunk
       ? `${titleChunk} · YouthKitbag Nuxt`
       : 'YouthKitbag Nuxt - Inventory, Trade, Aquire, Reporting for all your youth kit',
+  meta: [
+    {
+      name: 'description',
+      content: 'Your ultimate toolkit for youth activities.',
+    },
+  ],
 });
 </script>
