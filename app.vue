fcfb9638fa9bf8a82b399b1f23577c91b3d46380
Philip Moir <phil@captur.ai>
2025-05-01 00:19:48 +0100

    Set default title on page

--- app.vue
+++ app.vue
@@ -6,3 +6,12 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+useHead({
+  titleTemplate: (titleChunk) =>
+    titleChunk
+      ? `${titleChunk} · YouthKitbag Nuxt`
+      : 'YouthKitbag Nuxt - Inventory, Trade, Aquire, Reporting for all your youth kit',
+});
+</script>
