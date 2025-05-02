85d6bf8b15f7e9ce4351c31662ff0e8b3f055ec2
Philip Moir <phil@captur.ai>
2025-05-03 00:30:48 +0100

    Correct reference to login page rather than signin

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -23,7 +23,7 @@
       </ul>
     </div>
     <div class="navbar-end">
-      <a href="/signin" class="btn text-base">Log In</a>
+      <a href="/login" class="btn text-base">Log In</a>
       <div class="dropdown dropdown-end">
         <div tabIndex="{0}" role="button" class="btn btn-ghost lg:hidden">
           <svg
